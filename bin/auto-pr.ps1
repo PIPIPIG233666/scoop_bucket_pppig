158adec6660da6c4f5bb023fb39f110b9a0b95f0
pppig236 <weikaik@umich.edu>
2023-08-07 07:51:06 -0400

    update and add zentimings

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "PIPIPIG233666/scoop_bucket_pppig:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
